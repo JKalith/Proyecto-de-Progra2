bcfc9f12e56a290c35be835779ac16d9e866af5e
diego422 <diegoherreralopez9@gmail.com>
2022-12-02 02:14:02 -0600

    prueba final

--- Documents/Universidad 2022/Programacion 1/AA Proyecto 2/Proyecto netbeans/proyectoN2/src/ProyectN2/ProyectoN2.java
+++ Documents/Universidad 2022/Programacion 1/AA Proyecto 2/Proyecto netbeans/proyectoN2/src/ProyectN2/ProyectoN2.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.mail.MessagingException;
-
+//Hola
 /**
  *
  * @author James Arias Avalos
